d3bc2624e722b839e7ec6db4cc43df60ea77e448
Raed Mardoum <raedmrdom@gmail.com>
2024-01-31 15:50:19 +0400

    Added components file

---
+++ src/components/copyright/Copyright.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './copyright.css';
+
+const Copyright = () => {
+  return (
+    <div>
+        Copyright
+    </div>
+  )
+}
+
+export default Copyright
---
+++ src/components/donationtab/Donationtab.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './donationtab.css';
+
+const Donationtab = () => {
+  return (
+    <div>
+        Donationtab
+    </div>
+  )
+}
+
+export default Donationtab
