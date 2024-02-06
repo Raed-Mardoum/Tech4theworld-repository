78ba4f9422161bc70afbbb02be925fe7c4b1a574
Raed Mardom <raedmrdom@gmail.com>
2024-02-06 23:36:53 +0400

    Added slideshow element

--- src/components/slideshow/Slideshow.jsx
+++ src/components/slideshow/Slideshow.jsx
@@ -3,8 +3,16 @@ import './slideshow.css';
 
 const Slideshow = () => {
   return (
-    <div className='slideshow'>
-        <img />
+    <div className='slider'>
+
+      <figure>
+        <img className="slide" src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=1420&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        <img className="slide" src="https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        <img className="slide" src="https://images.unsplash.com/photo-1446776653964-20c1d3a81b06?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        <img className="slide" src="https://images.unsplash.com/photo-1504610926078-a1611febcad3?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        <img className="slide" src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=1420&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        </figure>
+
     </div>
   )
 }
