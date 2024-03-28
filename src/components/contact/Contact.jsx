774bfb3ddc964fbd6348ad3e4d5dca4790819cb1
Raed Mardoum <raedmrdom@gmail.com>
2024-03-28 12:44:10 +0400

    Changed the contact and form styling and structure

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,13 +4,41 @@ import './contact.css';
 const Contact = () => {
   return (
     <div className='Contact-background'>
-        <h1>Contact us</h1>
+        <h1>Contact information</h1>
 
-        <h3>Email: </h3>
-        <p>raedmrdom@gmail.com</p>
+        <div className='contact-content'>
 
-        <h3>Phone number: </h3>
-        <p>050-2527425</p>
+          <div className='contact-content-div'>
+
+            <h3>Email: </h3>
+            <p>raedmrdom@gmail.com</p>
+
+            <h3>Phone number: </h3>
+            <p>050-2527425</p>
+
+          </div>
+
+          <div className='contact-content-div'>
+
+            <h3>Social: </h3>
+            <div className='social'>
+
+              <i className="fa fa-facebook-official" aria-hidden="true"></i>
+              
+              <i className="fa fa-twitter-square" aria-hidden="true"></i>
+              
+              <i className="fa fa-instagram" aria-hidden="true"></i>
+              
+              <i className="fa fa-youtube-play" aria-hidden="true"></i>
+
+            </div>
+
+          </div>
+
+        </div>
+
+
+
     </div>
   )
 }
--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -3,8 +3,24 @@ import './form.css';
 
 const Form = () => {
   return (
-    <div>
-        Form
+    <div className='forum'>
+      
+        <h1>Contact us</h1>
+
+        <form>
+          
+          <label>Name:</label>
+          <input type="text" placeholder='Enter name'></input>
+
+          <label>Email:</label>
+          <input type="email" placeholder='example@gmail.com'></input>
+
+          <label>Message:</label>
+          <textarea placeholder='Enter message...'></textarea>
+
+          <input type="submit"></input>
+
+        </form>
     </div>
   )
 }
